fecc881eaa88a9ee12e378cc0072e50c5d816c1f
Dustin Rouillard <code@dstn.to>
2023-02-26 14:41:06 -0700

    Make closed private again Remove debugging variable

--- src/utils/gateway.ts
+++ src/utils/gateway.ts
@@ -285,7 +285,7 @@ export class Gateway extends EventEmitter {
     this.resetConnectionThrottle();
   }
 
-  closed(): void {
+  private closed(): void {
     console.log(
       `%c${SOCKET_URL.includes('puff.social') ? SOCKET_URL.split('.')[0].split('//')[1] : 'Local'}%c Socket connection closed`,
       "padding: 10px; text-transform: capitalize; font-size: 1em; line-height: 1.4em; color: white; background: #151515; border-radius: 15px;",
@@ -298,6 +298,3 @@ export class Gateway extends EventEmitter {
 
 export const SOCKET_URL = typeof location != 'undefined' && ['localhost', 'dev.puff.social'].includes(location.hostname) ? (location.hostname == 'dev.puff.social' ? 'wss://flower.puff.social' : 'ws://127.0.0.1:9000') : 'wss://rosin.puff.social';
 export const gateway = typeof window != "undefined" && (['ws://127.0.0.1:9000', 'wss://flower.puff.social'].includes(SOCKET_URL) || ['stage.puff.social'].includes(location.hostname) ? new Gateway(SOCKET_URL, 'json', 'none') : new Gateway(SOCKET_URL));
-
-// @ts-ignore
-if (typeof window != 'undefined') window.socket = gateway.ws.close.bind(gateway.ws);
