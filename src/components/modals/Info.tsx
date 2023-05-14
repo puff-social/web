7d1ce89d62ead163570bd5f13e16334dd1d5fb56
Dustin Rouillard <code@dstn.to>
2023-05-13 18:47:34 -0600

    Update info

--- src/components/modals/Info.tsx
+++ src/components/modals/Info.tsx
@@ -1,6 +1,5 @@
 import { Dialog, Transition } from "@headlessui/react";
-import { useRouter } from "next/router";
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
 
 interface Props {
   modalOpen: boolean;
@@ -8,8 +7,6 @@ interface Props {
 }
 
 export function InfoModal({ modalOpen, setModalOpen }: Props) {
-  const router = useRouter();
-
   return (
     <Transition appear show={modalOpen} as={Fragment}>
       <Dialog
@@ -40,12 +37,13 @@ export function InfoModal({ modalOpen, setModalOpen }: Props) {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-screen-lg transform overflow-hidden rounded-2xl bg-white dark:bg-neutral-800 text-black dark:text-white p-6 text-left align-middle shadow-xl transition-all">
-                <div className="flex flex-col rounded-md space-y-3 justify-center">
+              <Dialog.Panel className="w-full max-w-screen-xl transform overflow-hidden rounded-2xl bg-white dark:bg-neutral-800 text-black dark:text-white p-6 text-left align-middle shadow-xl transition-all">
+                <div className="flex flex-col rounded-md space-y-4 justify-center">
+                  <p className="text-2xl font-bold">About</p>
                   <p className="w-[700px]">
-                    <span className="font-bold">Important:</span> This should
-                    work with any device running firmware X or later, and has
-                    been validated to work with firmware{" "}
+                    <span className="font-bold">Firmware Note:</span> This
+                    should work with any device running firmware X or later, and
+                    has been validated to work with firmware{" "}
                     <span className="font-bold text-teal-100 bg-gray-700 px-1 rounded-md">
                       Y
                     </span>
@@ -67,7 +65,7 @@ export function InfoModal({ modalOpen, setModalOpen }: Props) {
                     </span>{" "}
                   </p>
                   <p className="w-[700px]">
-                    <span className="font-bold">Also Important:</span> This will
+                    <span className="font-bold">Browser Note:</span> This will
                     only work on browsers that support the Bluetooth API, so
                     most likely if you don't use Google Chrome, you're probably
                     only here to watch. (iOS can use the Path Browser, however
@@ -75,58 +73,88 @@ export function InfoModal({ modalOpen, setModalOpen }: Props) {
                     soon)
                   </p>
 
-                  <img className="w-96 rounded-md" src="/puff.webp" />
-                  <p className="w-[700px]">
-                    I decided to build this because A friend of mine and I smoke
-                    a lot over discord, we both have a peak pro, and while not
-                    super easy it is possible to interact with the device
-                    functions over BLE initially started by digging through the
-                    react native bundle for the official web app, besides that I
-                    just love realtime applications, so I built a{" "}
-                    <a
-                      className="text-blue-700 dark:text-blue-400"
-                      target="_blank"
-                      href="https://github.com/dustinrouillard/puffsocial-gateway"
-                    >
-                      socket server
-                    </a>{" "}
-                    in elixir to facilitate the synchronization, and threw
-                    together a rudimentary web app to allow us to sync our dab
-                    sessions with multiple people.
-                  </p>
+                  <hr className="opacity-20" />
 
-                  <p className="w-[700px]">
-                    After many iterations and plenty of bugs made and then
-                    fixed, I'm making this ready for anyone to use. You can set
-                    your public display name in user settings (separate from the
-                    device name, which is only used on the leaderboards)
-                  </p>
+                  <span className="flex flex-row space-x-4">
+                    <span className="w-96">
+                      <img className="w-fit rounded-md" src="/puff.webp" />
+                      <p className="flex text-sm italic opacity-40 break-words flex-wrap">
+                        Older design is pictured, updating this graphic soon.
+                      </p>
+                    </span>
+                    <span className="flex flex-col rounded-md space-y-4">
+                      <p className="w-[700px]">
+                        So I built this because a couple of friends and I would
+                        count down starting our puffcos over discord, and after
+                        some time we wanted an easier solution. I've dug into
+                        applications and recreated logic before so I started
+                        digging through the react native web bundles for the
+                        puffco app and got familiar with the bluetooth
+                        functionality.
+                      </p>
 
-                  <p className="w-[700px]">
-                    If you encounter any issues, let me know on discord
-                    (Dustin#1999 find me in the server below for dm) or Twitter
-                    (
-                    <a
-                      className="text-blue-700 dark:text-blue-400"
-                      href="https://twitter.com/dustinrouillard"
-                      target="_blank"
-                    >
-                      @dustinrouillard
-                    </a>
-                    )
-                  </p>
+                      <p className="w-[700px]">
+                        After getting the basic device functions triggering from
+                        my code and then working out pulling data from the
+                        devices, I put together a realtime socket server in
+                        Elixir (I used this as it's a great go-to for realtime
+                        concurrency and I'm familiar enough with it to
+                        accomplish the task) and then I put together this very
+                        react web app (using Next.js, Tailwind, and a few other
+                        tools. See the source below for more)
+                      </p>
 
-                  <p className="w-[700px]">
-                    Join our{" "}
-                    <a
-                      className="text-blue-700 dark:text-blue-400"
-                      target="_blank"
-                      href="https://discord.gg/M4uYMyU7bC"
-                    >
-                      discord server
-                    </a>
-                    , maybe we can sesh sometime :)
-                  </p>
+                      <p className="w-[700px]">
+                        If you encounter any issues, let us know in the discord
+                        below or you can DM me on Twitter (
+                        <a
+                          className="text-blue-700 dark:text-blue-400"
+                          href="https://twitter.com/dustinrouillard"
+                          target="_blank"
+                        >
+                          @dustinrouillard
+                        </a>
+                        ) or Instagram (
+                        <a
+                          className="text-blue-700 dark:text-blue-400"
+                          href="https://dstn.pics"
+                          target="_blank"
+                        >
+                          @dstn.pics
+                        </a>
+                        )
+                      </p>
+
+                      <p className="w-[700px]">
+                        Join our{" "}
+                        <a
+                          className="text-blue-700 dark:text-blue-400"
+                          target="_blank"
+                          href="https://discord.gg/M4uYMyU7bC"
+                        >
+                          Discord
+                        </a>{" "}
+                        and become apart of our growing community, maybe we can
+                        sesh up sometime :)
+                      </p>
+
+                      <p className="w-[700px]">
+                        Follow our{" "}
+                        <a
+                          className="text-blue-700 dark:text-blue-400"
+                          target="_blank"
+                          href="https://discord.gg/M4uYMyU7bC"
+                        >
+                          Instagram
+                        </a>{" "}
+                        account, we post on there with updates and news, plus we
+                        share the communities posts on our stores, and they're
+                        always nice to see.
+                      </p>
+                    </span>
+                  </span>
+
+                  <hr className="opacity-20" />
 
                   <p className="w-[700px] flex flex-col">
                     The source for the application and server side code is open,
@@ -142,16 +170,6 @@ export function InfoModal({ modalOpen, setModalOpen }: Props) {
                       </a>
                     </span>
                     <span>
-                      Realtime Server:{" "}
-                      <a
-                        className="text-blue-700 dark:text-blue-400"
-                        target="_blank"
-                        href="https://github.com/dustinrouillard/puffsocial-gateway"
-                      >
-                        dustinrouillard/puffsocial-gateway
-                      </a>
-                    </span>
-                    <span>
                       API:{" "}
                       <a
                         className="text-blue-700 dark:text-blue-400"
@@ -161,14 +179,24 @@ export function InfoModal({ modalOpen, setModalOpen }: Props) {
                         dustinrouillard/puffsocial-api
                       </a>
                     </span>
+                    <span>
+                      Realtime:{" "}
+                      <a
+                        className="text-blue-700 dark:text-blue-400"
+                        target="_blank"
+                        href="https://github.com/dustinrouillard/puffsocial-gateway"
+                      >
+                        dustinrouillard/puffsocial-gateway
+                      </a>
+                    </span>
                   </p>
 
                   <p className="w-[700px] italic text-sm">
-                    If you're reading this and work at Puffco, I just want to
-                    give cool tools to the community, would love to see a group
-                    sesh feature in the app (maybe we can chat about that) :) --
-                    If you have an issue with this or want me to change
-                    anything, reach out to me (contact@puff.social)
+                    Shoutout to Puffco for being so awesome and allowing me to
+                    run this platform, would love to work together on something
+                    in the future. React out if you wanna work together or wanna
+                    talk about how this works{" "}
+                    <a href="mailto:contact@puff.social">contact@puff.social</a>
                   </p>
                 </div>
                 <button
