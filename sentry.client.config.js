814d55b486b303e60fc5a19864e03b194f51efc1
Dustin Rouillard <code@dstn.to>
2023-05-28 22:00:53 -0600

    Fix sentry env

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,9 +2,13 @@ import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init({
-  dsn:
-    SENTRY_DSN ||
-    "https://c1c7ef7c7625418f992607458fc34c28@o1220194.ingest.sentry.io/4504790555361280",
-  tracesSampleRate: 1.0,
-});
+if (
+  typeof window != "undefined" &&
+  ["puff.social", "beta.puff.social"].includes(window.location.hostname)
+)
+  Sentry.init({
+    dsn:
+      SENTRY_DSN ||
+      "https://c1c7ef7c7625418f992607458fc34c28@o1220194.ingest.sentry.io/4504790555361280",
+    tracesSampleRate: 1.0,
+  });
