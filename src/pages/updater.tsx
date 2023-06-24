1bc8e4bad9efe35998796f9214bbd91f71f249e9
Dustin Rouillard <code@dstn.to>
2023-06-23 23:27:07 -0600

    Remove link from header

--- src/pages/updater.tsx
+++ src/pages/updater.tsx
@@ -199,9 +199,7 @@ export default function Updater() {
       <MainMeta pageName="Updater" />
 
       <div className="flex flex-col m-4 z-10 cursor-pointer">
-        <h1 className="text-4xl font-bold" onClick={() => router.push("/")}>
-          puff.social
-        </h1>
+        <h1 className="text-4xl font-bold">puff.social</h1>
         <h3 className="text font-bold">
           Firmware Tool -{" "}
           <a className="underline" href="https://puffco.app">
